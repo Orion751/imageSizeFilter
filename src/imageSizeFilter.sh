e751ccdd1bdeda8af4720668f84bdbc179693075
Orion751 <Orion751@users.noreply.github.com>
2016-05-31 04:02:38 -0400

    Added imageSizeFilter.sh

---
+++ src/imageSizeFilter.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# syntax: imageSizeFilter <smallImageDirectory> <bigImageDirectory>
+
+: '
+Pseudocode
+
+
+
+'
+verbose=false
+
+while getopts ":v" opt; do
+    case $opt in
+        v)
+            verbose=true
+
+            shift $((OPTIND-1))
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+
+            exit 1
+            ;;
+    esac
+done
+
+smallImageDirectory="$1"
+bigImageDirectory="$2"
+

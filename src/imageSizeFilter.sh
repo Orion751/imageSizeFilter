0d9df457a76a58c372fd59d5709e31e2f10a02b7
Orion751 <Orion751@users.noreply.github.com>
2016-06-21 22:18:06 -0400

    Added TODO Comments

--- src/imageSizeFilter.sh
+++ src/imageSizeFilter.sh
@@ -38,3 +38,6 @@ mkdir "$smallImageDirectory" "$bigImageDirectory"
 #find images -type f -exec identify -format '%w %h %i' '{}' \; 2>&1 | awk '$1<500 && $2<500' 1>/dev/null 2>&1 | tee log3.txt
 #foo=250; cat imageSizesLog.txt | awk '($1=='$foo' && $2>='$foo') || ($2=='$foo' && $1>='$foo')' | cut -d \  -f 3 | xargs eog
 
+# TODO Find the minimum width and minimum height of usuable images
+# TODO Get the folders that only contain valid images for MVP?
+
